dc749b355b75d9b01a7977a7a4c3a1a2724fb127
Yevhenii Dodiak <dodiak1996@gmail.com>
2021-09-27 22:23:13 +0200

    bag fix

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ import {openModal} from './modules/modal';
 
 window.addEventListener('DOMContentLoaded', function() {
 
-  const modalTimerId = setTimeout( () => openModal('.modal', modalTimerId), 300000);
+  const modalTimerId = setTimeout( () => openModal('.modal', modalTimerId), 30000);
 
   tabs(".tabheader__item", ".tabcontent", ".tabheader__items","tabheader__item_active");
   modal('[data-modal]', '.modal', modalTimerId);
